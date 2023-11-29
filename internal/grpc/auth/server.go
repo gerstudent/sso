b10f57b6de7366f2d24e9338be344e25684b0a59
gerstudent <gkolcherin@gmail.com>
2023-11-29 11:08:59 +0300

    Initial commit

---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,45 @@
+package grpc
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	// Сгенерированный код
+)
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+	auth Auth
+}
+
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appID int,
+	) (token string, err error)
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+}
+
+func Register(gRPCServer *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPCServer, &serverAPI{auth: auth})
+}
+
+func (s *serverAPI) Login(
+	ctx context.Context,
+	in *ssov1.LoginRequest,
+) (*ssov1.LoginResponse, error) {
+	// TODO
+}
+
+func (s *serverAPI) Register(
+	ctx context.Context,
+	in *ssov1.RegisterRequest,
+) (*ssov1.RegisterResponse, error) {
+	// TODO
+}
